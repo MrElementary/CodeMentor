748a0390b6968a0bcb4b5c2f0749e457b51f972f
Wihan Stoltz <112081989+MrElementary@users.noreply.github.com>
2023-05-06 11:37:15 +0200

    test suite
    Test suite python script created to generate a test for sample cases of the resister_networks.py script

---
+++ Section C: Code Challenge/test_suite.py
@@ -0,0 +1,25 @@
+"""unittest module used for the test suite,
+    as well as our main driver code to be tested
+    imported"""
+import unittest
+import resistor_networks
+
+# Tests based on the unittest module using example data
+class test_suite(unittest.TestCase):
+    def test_resistor_value(self):
+        input_data = resistor_networks.process_resistors("(10, [20, 30])")
+        self.assertEqual(input_data, 22.0, "Failed test, incorrect outcome")
+
+        input_data = resistor_networks.process_resistors("[10, (20, 30)]")
+        self.assertEqual(input_data, 8.3, "Failed test, incorrect outcome")
+
+        input_data = resistor_networks.process_resistors("([10, 20], (30, 40))")
+        self.assertEqual(input_data, 76.7, "Failed test, incorrect outcome")
+
+        input_data = resistor_networks.process_resistors("(1, [12, 4, (1, [10, (2, 8)])])")
+        self.assertEqual(input_data, 3.0, "Failed test, incorrect outcome")
+
+        input_data = resistor_networks.process_resistors("(6, [8, (4, [8, (4, [6, (8, [6, (10, 2)])])])])")
+        self.assertEqual(input_data, 10.0, "Failed test, incorrect outcome")
+
+unittest.main()
