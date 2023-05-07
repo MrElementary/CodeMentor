5d1bbadd155c5341dcbc5b03d8b83ed46e51124c
Wihan Stoltz <112081989+MrElementary@users.noreply.github.com>
2023-05-07 12:35:40 +0200

    Grammar updates

--- Section C: Code Challenge/test_suite.py
+++ Section C: Code Challenge/test_suite.py
@@ -4,9 +4,11 @@
 import unittest
 import resistor_networks
 
-# Tests based on the unittest module using example data
-class test_suite(unittest.TestCase):
+class TestSuite(unittest.TestCase):
+    """ Class for tests based on the unittest module using example data"""
+
     def test_resistor_value(self):
+        """ Function for tests based on the unittest module using example data"""
 
         # Samples for correct outcomes and calculations
         input_data = resistor_networks.process_resistors("(10, [20, 30])")
@@ -21,7 +23,8 @@ class test_suite(unittest.TestCase):
         input_data = resistor_networks.process_resistors("(1, [12, 4, (1, [10, (2, 8)])])")
         self.assertEqual(input_data, 3.0, "Failed test, incorrect outcome")
 
-        input_data = resistor_networks.process_resistors("(6, [8, (4, [8, (4, [6, (8, [6, (10, 2)])])])])")
+        input_data = resistor_networks.process_resistors(
+                            "(6, [8, (4, [8, (4, [6, (8, [6, (10, 2)])])])])")
         self.assertEqual(input_data, 10.0, "Failed test, incorrect outcome")
 
         # Samples for error handling of incorrect input
